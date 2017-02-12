2cbefbb882392dffc1c7900355eb24ff96fad70e
charmasaur <harry.slatyer@gmail.com>
2017-02-12 12:48:30 +0000

    Algorithmia website commit

--- src/crossword_extractor.py
+++ src/crossword_extractor.py
@@ -86,7 +86,7 @@ def get_cw_mask(input):
 # orthogonal truncated crossword
 def get_cw_orth_trunc(input):
     mask = get_cw_mask(input)
-    return len(np.nonzero(mask)[0])
+    return np.nonzero(mask)
 
     # get angle and rotate appropriately
     angle = get_angle_hough(mask)
