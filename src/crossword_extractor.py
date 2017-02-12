c15cd8a1cddd17d443e20bd7cea14d061b686502
charmasaur <harry.slatyer@gmail.com>
2017-02-12 15:40:23 +0000

    Algorithmia website commit

--- src/crossword_extractor.py
+++ src/crossword_extractor.py
@@ -82,7 +82,7 @@ def get_cw_mask(input):
     outputMask = mask[1:1+input.shape[0], 1:1+input.shape[1]]
     
     #outputMask.convertTo(outputMask, CV_8UC1, 255.);
-    return outputMask
+    return outputMask*255.
 
 # orthogonal truncated crossword
 def get_cw_orth_trunc(input):
