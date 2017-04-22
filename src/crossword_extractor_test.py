482fafaebd5716546122f06729f7c55a1ed6f79c
Harry Slatyer <harry.slatyer@gmail.com>
2017-04-22 10:53:18 +1000

    Figure out number of tests automatically

--- src/crossword_extractor_test.py
+++ src/crossword_extractor_test.py
@@ -1,9 +1,10 @@
 #from . import crossword_extractor
 import base64
 import crossword_extractor
+import os
 
 def test_crossword_extractor():
-    for i in range(4):
+    for i in range(get_num()):
         print("Testing " + str(i) + "...")
         test(i)
         print("  Success")
@@ -14,6 +15,12 @@ def test(test_num):
     actual = crossword_extractor.apply({"b64data":b64data})
     expected = load_expected(test_num)
     assert_equal(expected, actual)
+
+def get_num():
+    num_files = len(os.listdir("test_data"))
+    if num_files % 2 == 1:
+        raise Exception("Odd number of files")
+    return num_files / 2
 
 def load_input(test_num):
     f = open("test_data/in" + str(test_num) + ".jpg", "rb")
